f305949736f36f133c48cf2ebee0c64ce0defd18
Abhilov Gupta <113055824+abhilov23@users.noreply.github.com>
2026-04-09 01:47:19 +0530

    Implemented the deleteEvent function

--- models/event.go
+++ models/event.go
@@ -99,3 +99,18 @@ func (event Event) Update() error {
 
 	return nil
 }
+
+
+func (event Event) Delete() error {
+	query := "DELETE FROM events WHERE id = ?"
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(event.ID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
