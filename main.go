322dca67faa5132ffa29d08c92f2e5d38bd26b8e
Abhilov Gupta <113055824+abhilov23@users.noreply.github.com>
2026-04-09 01:42:22 +0530

    added update events function to update the events

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"database/sql"
-	"net/http"
-	"strconv"
-
 	"github.com/abhilov23/gin_project/db"
-	"github.com/abhilov23/gin_project/models"
+	"github.com/abhilov23/gin_project/routes"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,68 +12,9 @@ func main() {
 	// here the default() is the gin router
 	server := gin.Default()
 
-	// http support different kinds of requests: GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS etc.
-	server.GET("/events", getEvents)    // this will get all the events
-	server.GET(`/events/:id`, getEvent) // here we are using a dynamic placeholder for the id i.e. :id for getting the ID
-	server.POST("/events", createEvent) // this will create a new event
+	// this RegisterRoutes() handles all the routes
+	routes.RegisterRoutes(server) 
 
+	
 	server.Run(":8080") // localhost 8080
-}
-
-// here the context is the request and response object which provides different methods and functions
-// to handle the request and response
-func getEvents(context *gin.Context) {
-	events, err := models.GetAllEvents()
-
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events, try again later"})
-		return
-	}
-	// here the JSON method is used to send the response,
-	// and gin.H is a shortcut for map[string]interface{}
-	context.JSON(http.StatusOK, events)
-}
-
-func getEvent(context *gin.Context) {
-	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64) //converting string into int64
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": "invalid event id"})
-		return
-	}
-
-	event, err := models.GetEventByID(eventId)
-
-	if err != nil {
-		if err == sql.ErrNoRows {
-			context.JSON(http.StatusNotFound, gin.H{"message": "Event not found"})
-			return
-		}
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event, try again later"})
-		return
-	}
-
-	context.JSON(http.StatusOK, event)
-}
-
-func createEvent(context *gin.Context) {
-	var event models.Event
-	// this will only accept the json data defined in the event struct
-	// but it is not strictly required and can be ignored
-	err := context.ShouldBindJSON(&event)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": "could not parse request data"})
-		return
-	}
-
-	event.UserID = 1
-
-	err = event.Save()
-
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events, try again later"})
-		return
-	}
-	context.JSON(http.StatusCreated, gin.H{"event created": event})
 }
