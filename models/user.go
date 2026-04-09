0ddace9f7a7c082f283cacb8d30488fd4be2e711
Abhilov Gupta <113055824+abhilov23@users.noreply.github.com>
2026-04-09 23:10:33 +0530

    added hashing password functionality

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/abhilov23/gin_project/db"
+import (
+	"github.com/abhilov23/gin_project/db"
+	"github.com/abhilov23/gin_project/utils"
+)
 
 type User struct {
 	ID       int64
@@ -17,7 +20,15 @@ func (u User) Save() error {
 	}
 
 	defer stmt.Close()
-	result, err := stmt.Exec(u.Email, u.Password)
+    
+	hashedPassword, err := utils.HashPassword(u.Password)
+    
+	if err != nil {
+		return err
+	}
+	
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 	if err != nil {
 		return err
 	}
